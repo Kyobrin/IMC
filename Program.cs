be1f87308a24541fd75ca2c7e15a536e8534568d
kyobrin <Zullisun@gmail.com>
2022-12-09 19:48:02 -0300

    IMC CALCULADORA
    Programa concluído

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿{
+
+double M, A, IMC;
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("|-----------------------------|");
+Console.Write("|");
+Console.WriteLine("CALCULADORA IMC              |");
+Console.WriteLine("|-----------------------------|");
+Console.ResetColor();
+
+
+Console.WriteLine("MASSA (KG): ");
+M = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("ALTURA (M): ");
+A = Convert.ToDouble (Console.ReadLine());
+IMC = M / Math.Pow(A, 2);
+Console.WriteLine($"\nIMC: {IMC:N2} ");
+if (IMC < 17)
+{
+    Console.WriteLine("Muito abaixo do peso");
+
+}
+    else if(IMC >=17 && IMC < 18.5)
+    {
+        Console.WriteLine("Abaixo so Peso");
+    
+
+    }
+        else if (IMC >= 18.5 && IMC < 25)
+        {
+            Console.WriteLine("Peso ideal!");
+
+
+        }
+            else if (IMC >= 25 && IMC < 30)
+            {
+                Console.WriteLine("Sobrepeso");
+
+
+
+            }
+                else if (IMC >= 30 && IMC < 35)
+                {
+                    Console.WriteLine("Sobrepeso!");
+
+
+
+                }
+                    else if (IMC >= 35 && IMC < 40)
+                    {
+                        Console.WriteLine("Obesidade Severa, procure um hospital");
+
+
+                    }
+                        else
+                        {
+                            Console.WriteLine("Obesidade morbida, vá agora no medico. Por favor!");
+
+
+                        }
+}
+  
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# IMC
- 
+- #IMC
+Programa consiste em uma calculadora IMC, Com o intuito de calcular e informar o usuário se está sobrepeso!
+Obrigado por utilizar o programa!
